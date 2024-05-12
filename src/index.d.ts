8c0c58951bb15be4ab890f51e18cb13612d36411
Ryan Brown <ryanbrown4610@gmail.com>
2024-05-12 13:28:01 -0500

    Adjusting types

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,13 +3,22 @@ declare module 'react-range-slider-input' {
   import React from 'react';
 
   export interface RangeSliderInputProps {
+    id?: string;
+    className?: string;
     min: number;
     max: number;
     step?: number;
-    value: number;
-    onChange: (value: number) => void;
-    label: string;
-    className?: string;
+    defaultValue?: number[];
+    value?: number[];
+    onInput?: (value: number[]) => void;
+    onThumbDragStart?: () => void;
+    onThumbDragEnd?: () => void;
+    onRangeDragStart?: () => void;
+    onRangeDragEnd?: () => void;
+    disabled?: boolean;
+    rangeSlideDisabled?: boolean;
+    thumbsDisabled?: boolean;
+    orientation?: 'horizontal' | 'vertical';
   }
 
   export default function RangeSliderInput(props: RangeSliderInputProps): JSX.Element;
