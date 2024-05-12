ca713079fa0f6ce02ed7dafaa77dbb8fc6309391
Ryan Brown <ryanbrown4610@gmail.com>
2024-05-12 12:59:09 -0500

    Removing dist and instead using prepare

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
-var _RangeSlider = _interopRequireDefault(require("./components/RangeSlider"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-var _default = exports.default = _RangeSlider.default;
